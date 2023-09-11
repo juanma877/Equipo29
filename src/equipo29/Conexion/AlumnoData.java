85b00773a118266d1df776d014c01ab2dab02e2f
luisagote <agoteluisalberto@gmail.com>
2023-09-11 18:22:40 -0300

    Update AlumnoData.java
    Se  generaron los métodos para la clase AlumnoData.

--- src/equipo29/Conexion/AlumnoData.java
+++ src/equipo29/Conexion/AlumnoData.java
@@ -17,38 +17,37 @@ public class AlumnoData {
 
     private Connection con = null;
 
-    public AlumnoData(Conexion conexion) {
-        this.con = Conexion.buscarConexion();
-
+    public  AlumnoData(Conexion conexion) {
+        con = conexion.buscarConexion();
     }
 
-    public AlumnoData() {
+  
         
-    }
+    
 
     public void guardarAlumno(Alumno alumno) {
         String sql = "INSERT INTO alumno(dni, apellido, nombre, fechaNacimiento, estado) VALUES (?, ?, ?, ?, ?)";
-        try {
-            PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-            ps.setInt(1, alumno.getDni());
-            ps.setString(2, alumno.getApellido());
-            ps.setString(3, alumno.getNombre());
-            ps.setDate(4, alumno.getFechaNacimiento());
-            ps.setBoolean(5, alumno.isEstado());
-
-            int affectedRows = ps.executeUpdate();
-            if (affectedRows == 0) {
-                System.out.println("No se pudo insertar el registro.");
-            } else {
-                ResultSet rs = ps.getGeneratedKeys();
-                if (rs.next()) {
-                    alumno.setIdAlumno(rs.getInt(1));
+        
+            try (PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+                ps.setInt(1, alumno.getDni());
+                ps.setString(2, alumno.getApellido());
+                ps.setString(3, alumno.getNombre());
+                ps.setDate(4, alumno.getFechaNacimiento());
+                ps.setBoolean(5, alumno.isEstado());
+                
+                int affectedRows = ps.executeUpdate();
+                if (affectedRows == 0) {
+                    System.out.println("No se pudo insertar el registro.");
                 } else {
-                    System.out.println("No se pudo obtener el ID generado.");
+                    ResultSet rs = ps.getGeneratedKeys();
+                    if (rs.next()) {
+                        alumno.setIdAlumno(rs.getInt(1));
+                    } else {
+                        System.out.println("No se pudo obtener el ID generado.");
+                    }
+                    JOptionPane.showMessageDialog(null, "Alumno Guardado");
                 }
-                JOptionPane.showMessageDialog(null, "Alumno Guardado");
-            }
-            ps.close();
+           
         } catch (SQLException ex) {
             JOptionPane.showMessageDialog(null, "Error al conectase a la base de datos");
         }
@@ -101,25 +100,24 @@ public class AlumnoData {
         String sql = "SELECT dni, apellido, nombre, fechaNacimineto FROM alumno WHERE idAlumno=? AND estado=1";
         Alumno alumno = null;
 
-        try {
-            PreparedStatement ps = con.prepareStatement(sql);
-            ps.setInt(1, id);
-            ResultSet rs = ps.executeQuery();
-            if (rs.next()) {
-                alumno = new Alumno();
-                alumno.setIdAlumno(id);
-                alumno.setDni(rs.getInt("dni"));
-                alumno.setApellido(rs.getString("apellido"));
-                alumno.setNombre(rs.getString("nombre"));
-                alumno.setFechaNacimiento(rs.getDate("fechaNacimiento"));
-                alumno.setEstado(true);
-
-            } else {
-                JOptionPane.showMessageDialog(null, "Este alumno no existe como activo en la base de datos ");
-
-            }
-            ps.close();
-
+        
+            try (PreparedStatement ps = con.prepareStatement(sql)) {
+                ps.setInt(1, id);
+                ResultSet rs = ps.executeQuery();
+                if (rs.next()) {
+                    alumno = new Alumno();
+                    alumno.setIdAlumno(id);
+                    alumno.setDni(rs.getInt("dni"));
+                    alumno.setApellido(rs.getString("apellido"));
+                    alumno.setNombre(rs.getString("nombre"));
+                    alumno.setFechaNacimiento(rs.getDate("fechaNacimiento"));
+                    alumno.setEstado(true);
+                    
+                } else {
+                    JOptionPane.showMessageDialog(null, "Este alumno no existe como activo en la base de datos ");
+                    
+                }
+            
         } catch (SQLException ex) {
             JOptionPane.showMessageDialog(null, "Error al conectase a la base de datos");
         }
@@ -131,24 +129,24 @@ public class AlumnoData {
         String sql = "SELECT idAlumno,dni, apellido, nombre, fechaNacimineto FROM alumno WHERE dni=? AND estado=1";
         Alumno alumno = null;
 
-        try {
-            PreparedStatement ps = con.prepareStatement(sql);
-            ps.setInt(1, dni);
-            ResultSet rs = ps.executeQuery();
-            if (rs.next()) {
-                alumno = new Alumno();
-                alumno.setIdAlumno(rs.getInt("idAlumno"));
-                alumno.setDni(rs.getInt("dni"));
-                alumno.setApellido(rs.getString("apellido"));
-                alumno.setNombre(rs.getString("nombre"));
-                alumno.setFechaNacimiento(rs.getDate("fechaNacimiento"));
-                alumno.setEstado(true);
-
-            } else {
-                JOptionPane.showMessageDialog(null, "Este alumno no existe como activo en la base de datos ");
-
+        
+            try (PreparedStatement ps = con.prepareStatement(sql)) {
+                ps.setInt(1, dni);
+                ResultSet rs = ps.executeQuery();
+                if (rs.next()) {
+                    alumno = new Alumno();
+                    alumno.setIdAlumno(rs.getInt("idAlumno"));
+                    alumno.setDni(rs.getInt("dni"));
+                    alumno.setApellido(rs.getString("apellido"));
+                    alumno.setNombre(rs.getString("nombre"));
+                    alumno.setFechaNacimiento(rs.getDate("fechaNacimiento"));
+                    alumno.setEstado(true);
+                    
+                } else {
+                    JOptionPane.showMessageDialog(null, "Este alumno no existe como activo en la base de datos ");
+                    
+                
             }
-            ps.close();
         } catch (SQLException ex) {
             JOptionPane.showMessageDialog(null, "Error al conectase a la base de datos");
         }
@@ -160,22 +158,21 @@ public class AlumnoData {
         String sql = "SELECT idAlumno,dni, apellido, nombre, fechaNacimineto FROM alumno WHERE estado=1";
         ArrayList<Alumno> alumnos = new ArrayList<>();
 
-        try {
-            PreparedStatement ps=con.prepareStatement(sql);
-
-            ResultSet rs = ps.executeQuery();
-            while (rs.next()) {
-                Alumno alumno = new Alumno();
-                alumno.setIdAlumno(rs.getInt("idAlumno"));
-                alumno.setDni(rs.getInt("dni"));
-                alumno.setApellido(rs.getString("apellido"));
-                alumno.setNombre(rs.getString("nombre"));
-                alumno.setFechaNacimiento(rs.getDate("fechaNacimiento"));
-                alumno.setEstado(true);
-                alumnos.add(alumno);
-
+        
+            try (PreparedStatement ps = con.prepareStatement(sql)) {
+                ResultSet rs = ps.executeQuery();
+                while (rs.next()) {
+                    Alumno alumno = new Alumno();
+                    alumno.setIdAlumno(rs.getInt("idAlumno"));
+                    alumno.setDni(rs.getInt("dni"));
+                    alumno.setApellido(rs.getString("apellido"));
+                    alumno.setNombre(rs.getString("nombre"));
+                    alumno.setFechaNacimiento(rs.getDate("fechaNacimiento"));
+                    alumno.setEstado(true);
+                    alumnos.add(alumno);
+                    
+                
             }
-            ps.close();
         } catch (SQLException ex) {
             JOptionPane.showMessageDialog(null, "Error al conectase a la base de datos");
         }
@@ -183,3 +180,5 @@ public class AlumnoData {
     }
 
 }
+
+
