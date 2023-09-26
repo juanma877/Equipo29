57c7446924c1c04a5bdc457773d637bc11ea5678
mastranga <brunormastrangelo@gmail.com>
2023-09-26 19:25:25 -0300

    limpieza en el main

--- src/equipo29/ULPAlumnos.java
+++ src/equipo29/ULPAlumnos.java
@@ -1,34 +1,13 @@
-
 package equipo29;
 
-
-
-import java.sql.Date;
-import equipo29.Conexion.Conexion;
-import equipo29.Data.Alumno;
-import equipo29.Conexion.AlumnoData;
+import equipo29.vistas.PantallaPrincipal;
 
 
 public class ULPAlumnos {
-private AlumnoData alumnodata;
-private Conexion conexion;
     
     public static void main(String[] args) {
-        
-//        Alumno alumno1 = new Alumno(34862734,"Lopez","Ramiro",new Date(88,4,24),true);
-//        new ULPAlumnos().conectar(alumno1);
-//        System.out.println("Alumno "+ alumno1.getApellido()+ alumno1.getNombre()+ " guardado con exito");
-//       
-//       } 
-//        
-//       
-//        void conectar(Alumno alumno){
-//        
-//        conexion = new Conexion("jdbc:mariadb://localhost:3306/ulpalumnos?serverTimezone=UTC","root","");
-//        alumnodata= new AlumnoData(conexion);
-//        alumnodata.guardarAlumno(alumno);
-//        
-//      } 
+        PantallaPrincipal pp = new PantallaPrincipal();
+        pp.setVisible(true);
     }
 } 
 
